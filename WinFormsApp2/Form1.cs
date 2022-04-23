6052d2ad0c4235d7389424cacfc7dae13e2c8a99
Vladimir <vladimir.tereshin@urfu.me>
2022-04-23 12:27:01 +0500

    init

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Image ship;
+        public Player player;
+        
+        public Form1()
+        {
+            BackColor=Color.Aquamarine;
+            InitializeComponent();
+            // Initialise();
+        }
+        // public void Initialise()
+        // {
+        //     ship =
+        //         new Bitmap(@"C:\Users\User\Desktop\WinFormsApp1\WinFormsApp1\Ships\корабль.png");
+        //      ship = new Bitmap(ship, 100, 100);
+        //     var ship = new Ship(100, 100, pirateShip);
+        //     timer1.Start();
+        // }
+        
+    }
+}
---
+++ WinFormsApp2/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace WinFormsApp2
+{
+    public class Player
+    {
+        public int posX;
+        public int posY;
+        public Image image;
+        
+        public Player(int posX, int posY, Image image)
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.image = image;
+        }
+    }
+}
